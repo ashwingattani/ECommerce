f2b0a008ae9d93d63fa2ac00e089d027e8bd205a
Ashwin Gattani <gattani.ashwin@gmail.com>
2019-03-01 00:26:24 +0530

    CategoryViewModel to fetch json data

---
+++ ECommerce/ECommerce/ViewModels/CategoryViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryViewModel.swift
+//  ECommerce
+//
+//  Created by Ashwin Gattani on 01/03/19.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import Foundation
+
+let baseURL = "https://stark-spire-93433.herokuapp.com"
+
+class CategoryViewModel {
+    
+    public static let sharedInstance = CategoryViewModel()
+    
+    
+    
+}
